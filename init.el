a631e2ff505e4586488bbba8d0c644b56c0184c4
Roberto Guillén <robertoguillen@me.com>
2019-06-30 20:26:56 +0200

    Removed ciao mode

--- init.el
+++ init.el
@@ -266,7 +266,3 @@
 
 ;; Shortcut to artist mode.
 (global-set-key (kbd "C-c C-a") 'artist-mode)
-
-(let ((ciao-emacs-path "/usr/local/ciao/1.18.0/ciao_emacs/elisp/ciao-site-file.el"))
-  (if (file-exists-p ciao-emacs-path)
-      (load-file ciao-emacs-path)))
