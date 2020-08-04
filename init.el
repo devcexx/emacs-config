a71112f075be9f59dca1a707708e2d836e32d95b
Roberto Guillén <robertoguillen@me.com>
2020-08-04 22:37:33 +0200

    Improved tabs appearance

--- init.el
+++ init.el
@@ -77,8 +77,12 @@
   (setq fci-rule-use-dashes nil)
   (setq fci-rule-column 120)) ;; Keep disabled fci by default (gives problems with Company)
 
+;; Tabs
 (when (emacs-27)
   (global-tab-line-mode t)
+  (set-face-attribute 'tab-line-tab-current nil :foreground "#ffffff" :background nil :inherit 'highlight)
+  (set-face-attribute 'tab-line-highlight nil :foreground "#ffffff" :background "#bd158b")
+  (set-face-attribute 'tab-line-tab nil :background "gray35")
   (global-set-key (kbd "C-,") 'tab-line-switch-to-prev-tab)
   (global-set-key (kbd "C-.") 'tab-line-switch-to-next-tab))
 
