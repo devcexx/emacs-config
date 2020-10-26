3b4fdc2495c4a37d5f08aeea73d094189bc3a15f
Roberto Guillén <robertoguillen@me.com>
2020-10-26 01:47:43 +0100

    Added .tsx extension to be handled by typescript-mode

--- init.el
+++ init.el
@@ -362,6 +362,7 @@
   :hook
   (typescript-mode . lsp))
 (add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
 
 ;; Rust integration
 (use-package toml-mode
