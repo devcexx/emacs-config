d80dfbf2f12b5bf4923f83fbf3cd29274322d7fd
Roberto Guillén <robertoguillen@me.com>
2021-03-01 10:03:01 +0100

    Added some tweaks to early-init.el for preventing some issues with Emacs 28 native compilation, taken from https://github.com/jimeh/.emacs.d

---
+++ early-init.el
@@ -0,0 +1,8 @@
+(setq comp-speed 2)
+
+(setq comp-deferred-compilation-deny-list
+      '("\\(?:[/\\\\]\\.dir-locals\\.el$\\)"
+        ;; Don't native-compile *-authloads.el and *-pkg.el files as they
+        ;; seem to produce errors during native-compile.
+        "\\(?:[^z-a]*-autoloads\\.el$\\)"
+        "\\(?:[^z-a]*-pkg\\.el$\\)"))
