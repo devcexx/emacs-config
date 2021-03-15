7f1fd6d7585894e1f67c1bead01036a5dad90025
Roberto Guillén <robertoguillen@me.com>
2021-03-15 01:16:53 +0100

    Changed default tooltip background

--- config/config-theme.el
+++ config/config-theme.el
@@ -5,7 +5,9 @@
 ;; Default theme
 (use-package doom-themes
   :ensure t
-  :config (load-theme 'doom-Iosvkem t))
+  :config
+  (load-theme 'doom-Iosvkem t)
+  (set-face-attribute 'tooltip nil :background "#132236"))
 
 (provide 'config-theme)
 ;;; config-theme.el ends here
