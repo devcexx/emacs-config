ffbed0fbefc7ea1f3f70e9632df5e2ccc60b427e
Roberto Guillén <robertoguillen@me.com>
2021-06-13 21:31:25 +0200

    Moved basic configurations to another file.

---
+++ config/avoc-basics.el
@@ -0,0 +1,56 @@
+;;; avoc-basics.el --- Emacs basic configurations.
+;;;
+;;; Commentary:
+;;; Contains basic initial configurations to make Emacs
+;;; more useful.
+;;;
+;;; Code:
+
+;; Disable lock file creation.
+(setq create-lockfiles nil)
+
+;; Enable delete selection mode by default. I hate the default
+;; behaviour.
+(delete-selection-mode 1)
+
+;; Enable auto revert from disk when file changes.
+(global-auto-revert-mode t)
+
+;; Allow repeating C-u C-SPC.
+(setq set-mark-command-repeat-pop t)
+
+;; Prevent startup welcome Emacs buffer from being shown.
+(setq inhibit-startup-message t)
+
+;; Recursive minibuffers.
+(setq enable-recursive-minibuffers t)
+
+;; Prevent showing warning about cl package deprecated.
+(setq byte-compile-warnings '(cl-functions))
+
+;; Required by lsp-mode for increasing performance.
+(setq read-process-output-max (* 10 (* 1024 1024)))
+
+;; Disable right option modifier key on macOS
+(cond ((string-equal system-type "darwin")
+       (setq mac-right-option-modifier nil)))
+
+(setq custom-file (conf-rel-path "custom.el"))
+(load custom-file 'noerror)
+
+;; Enable desktop save mode
+(when (feature-enabled-p 'desktop-save-mode)
+  (desktop-save-mode 1))
+
+;; Attempt to fix a bug that produces sometimes the desktop-save-mode
+;; to fail reading the desktop file.
+(setq desktop-restore-forces-onscreen nil)
+
+;; Move temporal files to Emacs folder
+(setq backup-directory-alist
+      `((".*" . , (conf-rel-path "temp"))))
+(setq auto-save-file-name-transforms
+      `((".*" , (conf-rel-path "temp") t)))
+
+(provide 'avoc-basics)
+;;; avoc-basics.el ends here
--- init.el
+++ init.el
@@ -58,7 +58,6 @@ There are a few run modes that might fit different use cases:
 
     (light . (linum undo-tree company flycheck helm))))
 
-(setq create-lockfiles nil)
 (defmacro features-enabled-list ()
   "Return the list of features currently enabled by the run mode."
   `(alist-get emacs-run-mode run-modes-features))
@@ -103,48 +102,10 @@ There are a few run modes that might fit different use cases:
 ;; High initial GC threshold for speeding up Emacs load.
 (setq gc-cons-threshold 1000000000)
 
-;; Enable delete selection mode by default. I hate the default
-;; behaviour.
-(delete-selection-mode 1)
+(add-to-list 'load-path (conf-rel-path "config/"))
+(add-to-list 'load-path (conf-rel-path "config/winsys/"))
 
-;; Enable auto revert from disk when file changes.
-(global-auto-revert-mode t)
-
-;; Allow repeating C-u C-SPC.
-(setq set-mark-command-repeat-pop t)
-
-;; Prevent startup welcome Emacs buffer from being shown.
-(setq inhibit-startup-message t)
-
-;; Recursive minibuffers.
-(setq enable-recursive-minibuffers t)
-
-;; Prevent showing warning about cl package deprecated.
-(setq byte-compile-warnings '(cl-functions))
-
-;; Required by lsp-mode for increasing performance.
-(setq read-process-output-max (* 10 (* 1024 1024)))
-
-;; Disable right option modifier key on macOS
-(cond ((string-equal system-type "darwin")
-       (setq mac-right-option-modifier nil)))
-
-(setq custom-file (conf-rel-path "custom.el"))
-(load custom-file 'noerror)
-
-;; Enable desktop save mode
-(when (feature-enabled-p 'desktop-save-mode)
-  (desktop-save-mode 1))
-
-;; Attempt to fix a bug that produces sometimes the desktop-save-mode
-;; to fail reading the desktop file.
-(setq desktop-restore-forces-onscreen nil)
-
-;; Move temporal files to Emacs folder
-(setq backup-directory-alist
-      `((".*" . , (conf-rel-path "temp"))))
-(setq auto-save-file-name-transforms
-      `((".*" , (conf-rel-path "temp") t)))
+(require 'avoc-basics)
 
 ;; Init repositories
 (require 'package)
@@ -175,8 +136,6 @@ There are a few run modes that might fit different use cases:
 
   (setq use-package-verbose t))
 
-(add-to-list 'load-path (conf-rel-path "config/"))
-(add-to-list 'load-path (conf-rel-path "config/winsys/"))
 (if window-system
     (require 'avoc-winsys-graphic)
   (require 'avoc-winsys-none))
