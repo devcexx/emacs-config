9145f25664b720103f9bb8ed69c5b14364ce871f
Roberto Guillén <robertoguillen@me.com>
2021-06-13 20:35:37 +0200

    Removed fringe setting from graphical mode setting.

--- config/winsys/winsys-graphic.el
+++ config/winsys/winsys-graphic.el
@@ -1,9 +1,6 @@
 (defun emacs-setup-winsys-graphic ()
   "Enables specific Emacs configuration when running on a graphical interface (any of them)."
 
-  ;; Setup fringes
-  (set-fringe-mode '(8 . 0))
-
   ;; Set default fonts
   (add-to-list 'default-frame-alist
 	       '(font . "Hack-11")))
